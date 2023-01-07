11b005077e8a9bd23d50d3a833ef318dfacf9511
sercan-works <sercantas1026@gmail.com>
2023-01-08 02:52:51 +0300

    quicksort

--- app/app.py
+++ app/app.py
@@ -1,21 +1,30 @@
+import json
 from numbers import numbers
+
+from Sort import *
 
 checked_numbers = []
 
 
 
 def check_num(num):
-    if type(num) == int:
+    if type(num) == int :
         checked_numbers.append(num)
     else:
-        raise ValueError('NUMARA OLMAYAN DEĞER')
-    
-    
-    # if num.isdigit() == True:
-    #     print(number)
-    # else:
-    #     raise('Dizi içinde alfabetik öğe var!')
+        raise TypeError("Listede sadece sayısal değerler olmalıdır." )
+     
+    if len(numbers) <= 1:
+        raise Exception("Listeye birden fazla değer giriniz.")
 
+ 
+ 
+ 
 for number in numbers:
     check_num(number)
-    print(checked_numbers)
+    
+    
+print('Default___________:',numbers)    
+# print('Aritmetik sıralı__:\n',json.dumps(sorted(checked_numbers),indent=3))
+
+quickSort(checked_numbers,0,len(checked_numbers)-1)
+print('Aritmetik QuickSort sıralı__:\n',json.dumps(sorted(checked_numbers),indent=3))
