8415bf4f14e8e9a5d3ad8edc7ee7018aac48d777
Sercan <sercantas1026@gmail.com>
2023-01-08 12:45:49 +0300

    hj

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 import json
-from numbers import numbers
 
-from sort import *
+
+from Sort import *
 
 checked_numbers = []
 
+f = open('numbers.json')
+
+data = json.load(f)
 
 
 def check_num(num):
@@ -13,18 +16,20 @@ def check_num(num):
     else:
         raise TypeError("Listede sadece sayısal değerler olmalıdır." )
      
-    if len(numbers) <= 1:
+    if len(data['numbers']) <= 1:
         raise Exception("Listeye birden fazla değer giriniz.")
 
  
  
  
-for number in numbers:
+for number in data['numbers']:
     check_num(number)
     
     
-print('Default___________:',numbers)    
+print('Default___________:',data['numbers'])    
 # print('Aritmetik sıralı__:\n',json.dumps(sorted(checked_numbers),indent=3))
 
 quickSort(checked_numbers,0,len(checked_numbers)-1)
 print('Aritmetik QuickSort sıralı__:\n',json.dumps(sorted(checked_numbers),indent=3))
+
+f.close()
