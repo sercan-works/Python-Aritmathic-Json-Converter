56b9f434f48dbc57288afc98d40bfdb9264738e9
sercan-works <sercantas1026@gmail.com>
2023-01-09 11:43:07 +0300

    h

--- app/app.py
+++ app/app.py
@@ -4,31 +4,37 @@ from validators import validate_numbers
 
 checked_numbers = []
 
-f = open('numbers.json')
-
-data = json.load(f)
  
-def sort_numbers(data):
+def sort_numbers(data=None):
+    if data==None:
+        f = open('numbers.json')
+
+        data = json.load(f)
+        f.close()
+ 
+        numbers = data['numbers']
     
-    if len(data) <= 1: 
+    else:
+        numbers = data
+        
+    if len(numbers) <= 1: 
         raise Exception("Listeye birden fazla değer giriniz.")
     else:
-        
-        for number in data:
+
+        for number in numbers:
             validate_numbers(number)
             checked_numbers.append(number)
-            
+    
         return QuickSort(checked_numbers)
     
     
+    
 try:   
-    sort_numbers(data["numbers"])   
-    print('Default___________:',data['numbers'])    
-    # print('Aritmetik sıralı__:\n',json.dumps(sorted(checked_numbers),indent=3))
+    sort_numbers()  
     
     print('Aritmetik QuickSort sıralı__:\n',json.dumps(QuickSort(checked_numbers),indent=3))
 
-    f.close()
+        
 except:
     print('Bir hata oluştu.Listede en az 2 değer ve sadece number kullanılmalı.')
 
--- app/test.py
+++ app/test.py
@@ -5,7 +5,7 @@ from app import sort_numbers
 class TestApp(unittest.TestCase):    
     def setUp(self):
         print('- Test verileri hazırlanıyor...')
-        self.numbers = [5,1,8,7]
+        self.numbers = [5,1,8,9]
         print(self.numbers)
     def tearDown(self):
         print('- Test verileri siliniyor...')
@@ -14,6 +14,8 @@ class TestApp(unittest.TestCase):
     def test_app(self):
             
             self.assertTrue(sort_numbers(self.numbers))
+    def test_app_default(self):        
+            self.assertTrue(sort_numbers())
 
           
 if __name__ == '__main__':
