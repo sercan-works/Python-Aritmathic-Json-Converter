0c558afcc334d1868e914b413e38162e37260a60
Sercan <sercantas1026@gmail.com>
2023-01-09 03:40:41 +0300

    j

--- app/test.py
+++ app/test.py
@@ -1,23 +1,10 @@
 import unittest
-import json
 from Sort import *
 
 from app import sort_numbers
 
-f = open('numbers.json')
-data = json.load(f)
-
-
 class TestApp(unittest.TestCase):    
-           
-    def test_json_only_int(self):
-        for number in data['numbers']:        
-            self.assertEqual(type(number),int)
-            
-    def test_json_more_one_value(self):      
-            self.assertNotEqual(len(data["numbers"]),0)
-            self.assertNotEqual(len(data["numbers"]),1)
-            
+                       
     def test_app(self):   
             self.assertTrue(sort_numbers())
           
